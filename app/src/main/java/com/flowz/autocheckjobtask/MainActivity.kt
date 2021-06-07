9dfb3b59ece03b3630a5e1f70292ff23da726a38
AndroidPhrophet <ketuimevbore@yahoo.com>
2021-06-06 17:27:55 -0700

    Initial commit

---
+++ app/src/main/java/com/flowz/autocheckjobtask/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.flowz.autocheckjobtask
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        navController = findNavController( R.id.nav_host_fragment)
+
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+
+        return navController.navigateUp()|| super.onSupportNavigateUp()
+    }
+
+}
